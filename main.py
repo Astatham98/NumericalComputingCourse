aba4269e38606151f5a438c78ce3696f2cf94c82
Astatham98 <alex.statham1998@gmail.com>
2026-02-05 11:20:49 +0100

    fixed logical error and added cmap

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 def main(max_iters: int = 100) -> None:
     """Generate and plot the Mandelbrot set.
     Args: 
         max_iter (int): Maximum number of iterations.
     """
+    start_time = time.time()
     x_set, y_set = [-2, 1], [-1.5, 1.5]
     width = np.linspace(x_set[0], x_set[1], 100)
     height = np.linspace(y_set[0], y_set[1], 100)
@@ -18,20 +20,28 @@ def main(max_iters: int = 100) -> None:
     # Based on the 100x100 points, compute the mandelbrot set
     mandelbrot_set = np.zeros(points.shape, dtype=int)
     # Get n and c
-    for i, z in enumerate(points):
-        c = z
+    for i, c in enumerate(points):
+        z = 0
         for j in range(max_iters):
+            z = z**2 + c
             # Break if |Z| > 2
             if abs(z) > 2:
                 mandelbrot_set[i] = j
                 break
-            z = z**2 + c
+        else:
+            mandelbrot_set[i] = max_iters
+            
             
     # Reshape to 2D and plot        
-    mandelbrot_set = np.reshape(mandelbrot_set, (len(width), len(height)))
-    plt.imshow(mandelbrot_set)
+    mandelbrot_set = np.reshape(mandelbrot_set, (len(width), len(height))).T
+    
+    end_time = time.time()
+    print(f"Execution took: {end_time - start_time:.2f} seconds")
+    
+    plt.imshow(mandelbrot_set, cmap='twilight_shifted_r')
     plt.colorbar()
     plt.show()
 
 if __name__ == "__main__":
     main()
+    
