d382b398eaefc602fe5fa91ef411b32cac43bd2c
Alex Statham <alex.statham1998@gmail.com>
2026-02-26 14:31:21 +0100

    Added jit implementation (8x speedup)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from numba import jit
-from memory_profiler import profile
+from numba import jit, njit
+import cProfile
+from line_profiler import profile
+
 
 
 """
@@ -46,6 +48,20 @@ def w1_main(max_iters: int = 100,
     Returns:
         np.ndarray: Mandelbrot set values in a 2D array.
     """
+    def w1_f(points: np.ndarray[complex], mandelbrot_Set: np.ndarray[int], max_iters: int = 100) -> np.ndarray:
+            for i, c in enumerate(points):
+                z = 0
+                for j in range(max_iters):
+                    z = z**2 + c
+                    # Break if |Z| > 2
+                    if abs(z) > 2:
+                        mandelbrot_set[i] = j
+                        break
+                else:
+                    mandelbrot_set[i] = max_iters
+            return mandelbrot_set
+
+
     x_set, y_set = [-2, 1], [-1.5, 1.5]
     width = np.linspace(x_set[0], x_set[1], win_size)
     height = np.linspace(y_set[0], y_set[1], win_size)
@@ -58,21 +74,11 @@ def w1_main(max_iters: int = 100,
     # Based on the 100x100 points, compute the mandelbrot set
     mandelbrot_set = np.zeros(points.shape, dtype=int)
     # Get n and c
-    for i, c in enumerate(points):
-        z = 0
-        for j in range(max_iters):
-            z = z**2 + c
-            # Break if |Z| > 2
-            if abs(z) > 2:
-                mandelbrot_set[i] = j
-                break
-        else:
-            mandelbrot_set[i] = max_iters
+    mandelbrot_set = w1_f(points, mandelbrot_set, max_iters)
     # Reshape to 2D and plot        
     mandelbrot_set = np.reshape(mandelbrot_set, (len(width), len(height))).T
     return mandelbrot_set
 
-@timing
 def w2_main(max_iters: int = 100, 
         x_set: tuple = (-2.0, 1.0),    # Changed to tuple + floats
         y_set: tuple = (-1.5, 1.5),    # Changed to tuple + floats
@@ -86,7 +92,7 @@ def w2_main(max_iters: int = 100,
     Returns:
         np.ndarray: Mandelbrot set values in a 2D array.
     """
-
+    @timing
     def f(C: np.ndarray[complex], Z: np.ndarray, M: np.ndarray, max_iters: int) -> np.ndarray:
         for _ in range(max_iters):
             # Calculate the mask for every point in Z
@@ -189,26 +195,73 @@ def w2_scaling():
     plt.show()
     return sizes, times
 
+@timing
+@njit
+def w3_f(C: np.ndarray[complex], mandelbrot_set: np.ndarray, max_iters: int) -> np.ndarray:
+    for i, col in enumerate(C):
+        for j, c in enumerate(col):
+            z = 0j
+            for k in range(max_iters):
+                z = z**2 + c
+                # Break if |Z| > 2
+                if z. real *z. real + z. imag *z. imag > 4.0: 
+                    mandelbrot_set[i,j] = k
+                    break
+            else:
+                mandelbrot_set[i,j] = max_iters
+    return mandelbrot_set
+
+def w3_main(max_iters: int = 100, 
+        x_set: tuple = (-2.0, 1.0),    # Changed to tuple + floats
+        y_set: tuple = (-1.5, 1.5),    # Changed to tuple + floats
+        win_size: int = 100,
+        dtype: np.dtype = np.float64) -> np.ndarray :
+    """Generate and plot the Mandelbrot set.
+    Args: 
+        max_iter (int): Maximum number of iterations.
+        x_set (tuple): X-axis range.
+        y_set (tuple): Y-axis range.
+        win_size (int): Number of points in each axis.
+    Returns:
+        np.ndarray: Mandelbrot set values in a 2D array.
+    """
+    width = np.linspace(x_set[0], x_set[1], win_size, dtype=dtype)
+    height = np.linspace(y_set[0], y_set[1], win_size, dtype=dtype)
+    X, Y = np.meshgrid(width, height)
+    C = X + 1j * Y
+    
+    # Based on the 100x100 points, compute the mandelbrot set
+    mandelbrot_set = np.zeros(C.shape, dtype=np.int32)
+    # Compute the Mandelbrot set using the function f 
+    mandelbrot_set = w3_f(C, mandelbrot_set, max_iters)
+    return mandelbrot_set
 
 def benchmark_all(n_runs=3):
+    print('Week 1: Naive python implementatio ')
     median_w1, mandelbrot_set_w1 = benchmark(w1_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, n_runs=n_runs)
+    print('Week 2: numpy vectorization')
     median_w2, mandelbrot_set_w2 = benchmark(w2_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, n_runs=n_runs)
+    print('Week 3: Naive numba')
     median_w1_5, mandelbrot_set_w1_5 = benchmark(w_1_5_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, n_runs=n_runs)
-    if np.allclose(mandelbrot_set_w1, mandelbrot_set_w2):
-        print("All implementations produce the same result.")
-    else:
-        print("Results differ between implementations.")
-        diff = np.abs(mandelbrot_set_w1 - mandelbrot_set_w2)
-        print(f"Max difference: {np.max(diff)}")
+    print('Weel 3: optimized numba')
+    median_w3, mandelbrot_set_w3 = benchmark(w3_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, n_runs=n_runs)
+
+    return median_w1, median_w2, median_w1_5, median_w3
+
+def benchmark_dtype(n_runs):
+    median_w3_64, mandelbrot_set_w3 = benchmark(w3_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, np.float64, n_runs=n_runs)
+    median_w3_64, mandelbrot_set_w3 = benchmark(w3_main, 100, (-2.0, 1.0), (-1.5, 1.5), 1024, np.float32, n_runs=n_runs)
+
 
 if __name__ == "__main__":
     #seahorse = w_1_5_main(max_iters=500, x_set=(-0.8, -0.7), y_set=(0.05, 0.15), win_size=1024)
     #elephant = w_1_5_main(max_iters=500, x_set=(0.175, 0.375), y_set=(-0.1, 0.1), win_size=1024)
     #deep_seahorse = w_1_5_main(max_iters=2000, x_set=(-0.7487667139, -0.7487667078), y_set=(0.1236408449, 0.1236408510), win_size=1024)
-    #  mandelbrot_set= w_1_5_main(win_size=1024)
-    # plt.imshow(deep_seahorse, cmap='twilight_shifted_r')
+    #mandelbrot_set= w3_main(win_size=1024)
+    
+    benchmark_all()
+    # plt.imshow(w3_mandel, cmap='twilight_shifted_r')
     # plt.colorbar()
     # plt.show()
 
-    w2_memory_access()
     
